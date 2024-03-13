40febaa488bc06af23898befa6e37997d951c57b
Robert Hernandez <robert.hernandez@travelandnight.com>
2024-03-13 01:04:04 +0100

    Initial project files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+   <h1 class="fs-1 fw-bold text-primary">Hello World</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+ 
+  </header>
+
+  <RouterView />
+</template>
